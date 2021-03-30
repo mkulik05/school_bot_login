4d0ad42b57afeee15bd998e9c6aee92584b96ea7
Misha <mishakulik2@gmail.com>
2021-03-30 20:41:03 +0300

    add logger to main func

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const db = require("./db")
+const logger = require("./logger")("server")
 const fs = require('fs');
 var bodyParser = require('body-parser')
 const app = express()
@@ -11,13 +12,20 @@ const server = https.createServer({key: key, cert: cert }, app);
 var urlencodedParser = bodyParser.urlencoded({ extended: true })
 app.use('/static', express.static('static'));
 app.get('/', (req, res) => {
+  logger.info("new listener")
   res.redirect("/static/index.html")
 })
 app.post('/static/index.html', urlencodedParser, async (req, res) => {
-  res.send('Got a POST request');
-  res = req.body
-  await db({login: res.login, password: res.password}, res.tg_id)
-  console.log(req.body)
+  logger.info("post request")
+  try {
+    res.send('Got a POST request');
+    res = req.body
+    logger.info(res)
+    await db({login: res.login, password: res.password}, res.tg_id)
+    console.log(req.body)
+  } catch(e) {
+    logger.error(e)
+  }
 });
 server.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
